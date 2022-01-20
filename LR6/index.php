096ef8208db302983940418ed776dc607ba8780f
vgrechishnikov <v.grechishnikov@syberry.com>
2022-01-20 22:41:43 +0300

    Added LR6/index.php

--- LR6/index.php
+++ LR6/index.php
@@ -1,19 +1,36 @@
 <?php
-// Константы необходимых директорий.
-define("DIR_ROOT", $_SERVER["DOCUMENT_ROOT"]);
-define("DIR_IMG", "/img/");
-// Формирование массива с именами изображений.
-$img = array_splice(scandir(DIR_ROOT . DIR_IMG ."small"), 2);
+function upload_file($file) {
+    if ($file['name'] == '') {
+        echo 'Файл не выбран!<br>';
+        return;
+    }
+    if(copy($file['tmp_name'], 'img/'.$file['name']))
+        echo 'Файл успешно загружен<br>';
+    else
+        echo 'Ошибка загрузки файла<br>';
+}
 ?>
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Gallery_single_page</title>
+    <meta charset="cp1251"/>
+    <title>Загрузка файла на сервер</title>
 </head>
 <body>
-<? foreach ($img as $smallimg): ?>
-    <a href="<?= DIR_IMG . "big/" . $smallimg ?>" target="_blank"><img src="<?= DIR_IMG . "small/" . $smallimg ?>" alt="#"></a>
-<? endforeach; ?>
+<h1>Пример загрузки файла на сервер</h1>
+<?php
+if (isset($_FILES['file']) and str_ends_with($_FILES['file']['name'], '.jpg'))
+    upload_file($_FILES['file']);
+?>
+<form method="post" enctype="multipart/form-data"> <input type="file" name="file" />
+    <input type="submit" value="Загрузить файл!" />
+</form>
 </body>
 </html>
+<?php
+foreach (scandir('img/') as $file) {
+    if ($file !== '.' and $file !== '..') {
+        echo "<a href='img/$file' target='_blank'><img src='img/$file' style='width: 300px; margin: 5px'></a>";
+    }
+}
+?>
